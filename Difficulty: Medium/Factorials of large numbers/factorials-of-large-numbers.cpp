f8566247c100ebaca6d11156d6c6b5b6d2f2740e
Smriti Singh <smritisingh9118@gmail.com>
2026-07-14 23:10:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Factorials of large numbers/factorials-of-large-numbers.cpp
@@ -0,0 +1,27 @@
+// User function template for C++
+
+class Solution {
+  public:
+    vector<int> factorial(int n) {
+        // code here
+        vector<int>ans(1,1);
+        while(n>1)
+        {
+            int carry=0, res, size=ans.size();
+            for(int i=0;i<size;i++)
+            {
+                res=ans[i]*n+carry;
+                carry=res/10;
+                ans[i]=res%10;
+            }
+            while(carry)
+            {
+                ans.push_back(carry%10);
+                carry/=10;
+            }
+            n--;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
